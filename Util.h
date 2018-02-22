16bc2aa5ed965d8ae2c63f628c94216de349235c
Jonas Pohl <jonas.jp4c@gmail.com>
2018-02-22 20:32:45 +0100

    Aufteilung des Codes in verschiedene Dateien

---
+++ Util.h
@@ -0,0 +1,5 @@
+#include "Arduino.h"
+#include "Vars.h"
+#include "SharpIR.h"
+
+extern int readLeftFront(SharpIR sensor);
---
+++ Vars.h
@@ -0,0 +1,25 @@
+#define ena 9 //Rechter Motor
+#define enb 8
+#define IN1 45
+#define IN2 44
+#define IN3 43
+#define IN4 42
+
+#define LCDAddress 0x20
+#define LCDCols 20
+#define LCDRows 4
+
+#define IRFrontLeft A0  // 1080
+#define IRFrontRight A4 // 430
+#define IRLeftBack A6   // 430
+#define IRLeftFront A1  // 430
+
+
+#define grSclL A8
+#define grSclR A9
+
+#define LEDRed 13
+#define LEDGreen 11
+#define Piezo 7
+
+#define LightSensor A3
